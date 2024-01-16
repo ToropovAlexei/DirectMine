4f831e90902e290d37ac0120f21a3e80a4e9c3aa
Aleksey Toropov <aleksey.toropov@novakidschool.com>
2024-01-16 22:56:58 +0300

    sleep time updated

--- ChunksManager.cpp
+++ ChunksManager.cpp
@@ -181,7 +181,7 @@ void ChunksManager::AsyncProcessChunks()
 		UnloadFarChunks();
 		LoadChunks();
 		UpdateModifiedChunks();
-		std::this_thread::sleep_for(std::chrono::milliseconds(1));
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 }
 
