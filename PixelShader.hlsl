16bc26392b06a86dcaccc20bf080501b5e69b061
Aleksey Toropov <aleksey.toropov@novakidschool.com>
2024-01-19 10:02:38 +0300

    fog temporary disabled

--- PixelShader.hlsl
+++ PixelShader.hlsl
@@ -28,6 +28,7 @@ float CalculateFogFactor(float depth)
 float4 main(VertexOut pin) : SV_Target
 {
     float fogFactor = CalculateFogFactor(pin.Depth);
-    float4 finalColor = lerp(tex.Sample(splr, pin.TexC), FOG_COLOR, fogFactor);
+    //float4 finalColor = lerp(tex.Sample(splr, pin.TexC), FOG_COLOR, fogFactor);
+    float4 finalColor = tex.Sample(splr, pin.TexC);
     return finalColor;
 }
