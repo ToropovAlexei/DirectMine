a5deaf7ace38b974aab5942af8111b2929b02da1
ToropovAlexei <henschel37rus@gmail.com>
2024-02-05 19:41:32 +0300

    Chunks renderer fix

--- ChunkRenderer.cpp
+++ ChunkRenderer.cpp
@@ -58,7 +58,7 @@ void ChunkRenderer::BuildInputLayout()
 void ChunkRenderer::BuildSampler()
 {
     D3D11_SAMPLER_DESC samplerDesc = {};
-    samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+    samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
     samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
     samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
     samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
